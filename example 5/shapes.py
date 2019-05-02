4858f44446cde1cb292da297a059bcdedc0e2ae2
joanaluka <47768918+joanaluka@users.noreply.github.com>
2019-05-02 18:57:26 +0300

    Add files via upload

---
+++ example 5/shapes.py
@@ -0,0 +1,36 @@
+from shapes import
+class Circle:
+    def __init__(self,radius):
+            self.radius = radius
+    def area (self,a):
+            a = 3.14*r*r
+            print(a)
+    def circumference(self,c):
+            c=2*3.14*r
+            print(c)
+class Square:
+    def __init__(self,a):
+            self.Side=a
+    def area(self,d):
+             d=a*a
+             print(d)
+    def perimeter(self,k):
+            P=4*(k+K+K+K)
+            print(p)
+class Rectangle:
+    def __init__(self,l,w):
+            self.w=w
+            self.l=l
+            area=(w*l)
+            print(area)
+    def perimeter(self,l):
+            p=2*(l+w)
+            print(p)
+class Sphere:
+    def __init__(self,r):
+           self.r=r
+    def area(self,a):
+           a=4*(3.14*r*r)
+           print(a)
+    def volume(self,d):
+    	 d =4/3*(3.14*r*r*r)
